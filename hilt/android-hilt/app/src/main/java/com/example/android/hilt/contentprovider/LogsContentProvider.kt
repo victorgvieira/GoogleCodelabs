64de8d348ff281edc119df02c32eb77c124a3bff
victorgvieira <vggvieira@gmail.com>
2020-08-18 23:57:11 -0300

    :hammer: @EntryPoint annotation

---
+++ hilt/android-hilt/app/src/main/java/com/example/android/hilt/contentprovider/LogsContentProvider.kt
@@ -0,0 +1,111 @@
+package com.example.android.hilt.contentprovider
+
+import android.content.*
+import android.database.Cursor
+import android.net.Uri
+import com.example.android.hilt.data.LogDao
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.android.components.ApplicationComponent
+import java.lang.IllegalArgumentException
+import java.lang.IllegalStateException
+import java.lang.UnsupportedOperationException
+
+
+//DONE Create the ContentProvider class
+
+/** The authority of this content provider. **/
+private const val LOGS_TABLE = "logs"
+
+/** The authority of this content provider. **/
+private const val AUTHORITY = "com.example.android.hilt.provider"
+
+/** The match code for some items in the logs table. **/
+// NOTE: This is for query all items
+private const val CODE_LOGS_DIR = 1
+
+/** The match code for an item in the logs table. **/
+// NOTE: This is for query item by id
+private const val CODE_LOGS_ITEM = 2
+
+/**
+ * A ContentProvider that exposes the logs outside the application process.
+ * */
+class LogsContentProvider : ContentProvider() {
+
+    // DONE Create an entryPoint interface
+    @InstallIn(ApplicationComponent::class)
+    @EntryPoint
+    interface LogsContentProviderEntryPoint {
+        fun logDao(): LogDao
+    }
+
+    // DONE Create a method to access a value from an entryPoint
+    // NOTE: the Context must match the same type in the get method from EntryPointAccessors
+    // in this case ApplicationContext
+    private fun getLogDao(appContext: Context): LogDao {
+        val hiltEntryPoint = EntryPointAccessors.fromApplication(
+            appContext,
+            LogsContentProviderEntryPoint::class.java
+        )
+        return hiltEntryPoint.logDao()
+    }
+
+    private val matcher: UriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
+        addURI(AUTHORITY, LOGS_TABLE, CODE_LOGS_DIR)
+        addURI(AUTHORITY, "$LOGS_TABLE/*", CODE_LOGS_ITEM)
+    }
+
+    override fun onCreate(): Boolean = true
+
+
+    /**
+     * Queries all the logs or an individual log from the logs database.
+     * For the sake of this codelab, the logic has been simplified.
+     * */
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? {
+        val code: Int = matcher.match(uri)
+        return when (code) {
+            CODE_LOGS_DIR, CODE_LOGS_ITEM -> {
+                val appContext = context?.applicationContext ?: throw IllegalStateException()
+                val logDao: LogDao = getLogDao(appContext)
+
+                val cursor: Cursor? = when (code) {
+                    CODE_LOGS_DIR -> logDao.selectAllLogsCursor()
+                    else -> logDao.selectLogById(ContentUris.parseId(uri))
+                }
+                cursor?.setNotificationUri(appContext.contentResolver, uri)
+                cursor
+            }
+            else -> throw IllegalArgumentException("Unknow URI: $uri")
+        }
+    }
+
+    override fun getType(uri: Uri): String? {
+        throw UnsupportedOperationException("Only reading operations are allowed")
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        throw UnsupportedOperationException("Only reading operations are allowed")
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
+        throw UnsupportedOperationException("Only reading operations are allowed")
+    }
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<out String>?
+    ): Int {
+        throw UnsupportedOperationException("Only reading operations are allowed")
+    }
+}
--- hilt/android-hilt/app/src/main/java/com/example/android/hilt/data/LogDao.kt
+++ hilt/android-hilt/app/src/main/java/com/example/android/hilt/data/LogDao.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.hilt.data
 
+import android.database.Cursor
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
@@ -34,4 +35,11 @@ interface LogDao {
 
     @Query("DELETE FROM logs")
     fun nukeTable()
+
+    // DONE Add new methods with cursor as return type
+    @Query("SELECT * FROM logs ORDER BY id DESC")
+    fun selectAllLogsCursor(): Cursor
+
+    @Query("SELECT * FROM logs WHERE id = :id")
+    fun selectLogById(id: Long): Cursor?
 }
