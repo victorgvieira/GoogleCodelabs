05de5574e63685d4ecc0421260adf0d8c7f9c961
Victor Vieira <vggvieira@gmail.com>
2023-09-03 17:09:01 -0300

    :construction: 4 - Tweaking the UI

--- app/src/main/java/com/ggvieira/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/ggvieira/basicscodelab/MainActivity.kt
@@ -3,18 +3,25 @@ package com.ggvieira.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ggvieira.basicscodelab.ui.theme.BasicsCodelabTheme
 
+// TO_KNOWN: Activity remains the entry point to an Android app.
+// MainActivity is launched when the user opens the app
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // TO_KNOWN:  Use setContent to define your layout, but instead of using an XML file
+        // you call Composable functions within it.
         setContent {
             BasicsCodelabTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Victor")
                 }
             }
         }
@@ -31,16 +38,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text(
+            text = "Hello $name!",
+            // TO_KNOWN: Modifiers an optional modifier parameter that tell a UI element
+            // how to lay out, display, or behave within its parent layout.
+            // ex for padding: Modifier.padding().
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BasicsCodelabTheme {
-        Greeting("Android")
+        Greeting("Victor")
     }
 }
