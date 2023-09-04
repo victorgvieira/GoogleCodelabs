e09b8c1c901d2376c115940a66daa57a4b556c34
Victor Vieira <vggvieira@gmail.com>
2023-09-03 23:10:24 -0300

    :construction: 6 - Creating columns and rows

--- app/src/main/java/com/ggvieira/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/ggvieira/basicscodelab/MainActivity.kt
@@ -3,8 +3,12 @@ package com.ggvieira.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -30,26 +34,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp(modifier: Modifier = Modifier) {
-    // A surface container using the 'background' color from the theme
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Greeting(name = "Victor")
+private fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("Victor", "Compose")
+) {
+    // each row takes the minimum space it can and the preview does the same thing
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(
-            text = "Hello $name!",
-            // TO_KNOWN: Modifiers an optional modifier parameter that tell a UI element
-            // how to lay out, display, or behave within its parent layout.
-            // ex for padding: Modifier.padding().
-            modifier = modifier.padding(24.dp)
-        )
+fun Greeting(name: String) {
+    // A surface container using the 'background' color from the theme
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        // TO_KNOWN: Modifiers an optional modifier parameter that tell a UI element
+        // how to lay out, display, or behave within its parent layout.
+        // ex for padding: Modifier.padding().
+//        Modifiers can have overloads so, for example, you can specify different ways to create a padding.
+//        To add multiple modifiers to an element, you simply chain them.
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp)
+        ) {
+//         TO_KNOWN: The weight modifier makes the element fill all available space,
+//         making it flexible, pushing away the other elements that don't have a weight
+//         ,which are called inflexible. It also makes the fillMaxWidth modifier redundant.
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello,")
+                Text(text = "$name!")
+            }
+            ElevatedButton(onClick = { /*TODO*/ }) {
+                Text(text = "Show more")
+            }
+        }
     }
 }
 
@@ -61,7 +85,7 @@ fun GreetingPreview() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabTheme {
